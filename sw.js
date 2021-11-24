4667ef6d2e9ab29702c4954e1a41223b197e49ec
simonVonFelten <simon.von_felten@edu.univ-fcomte.fr>
2021-11-24 15:47:23 +0100

    new strategy

--- sw.js
+++ sw.js
@@ -24,6 +24,7 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
+	console.log("fetch");
   e.respondWith((async () => {
   	let ressource = e.request.url;
   	if(appShellFiles.includes(ressource)){
